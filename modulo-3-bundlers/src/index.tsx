8125c4a78379e7d0b631bdfc16184c4c1ff79d9f
Luis Villén <l.villenjaldon@gmail.com>
2021-10-07 21:23:15 +0200

    Adds image

--- modulo-3-bundlers/src/index.tsx
+++ modulo-3-bundlers/src/index.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { AverageComponent } from "./averageComponent";
 import { TotalScoreComponent } from './totalScoreComponent';
+import dnd1 from "./content/dnd1.png";
 
 ReactDOM.render(
   <div>
     <h1>Hello from React DOM</h1>
+    <img src={dnd1} />
     <AverageComponent />
     <TotalScoreComponent />
   </div>,
